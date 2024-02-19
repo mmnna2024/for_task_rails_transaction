bad98ef4b4a803c1b3f96194f91cdf972ddc482c
naomu <muramatsu2023@gmail.com>
2024-02-19 13:56:48 +0900

    second commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,11 +11,12 @@ class OrdersController < ApplicationController
 
   def create
     ActiveRecord::Base.transaction do
-      @order = current_user.orders.lock.build(order_params)
-        @order.save!
-        @order.update_total_quantity
-        # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
+      @order = current_user.orders.build.lock!(order_params)
+      @order.save!
+      @order.update_total_quantity
+      # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
     end
+    binding.pry
     redirect_to orders_path
   end
 

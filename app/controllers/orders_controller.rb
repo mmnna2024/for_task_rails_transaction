f30b794c0c10160ea36f35e2b0f6a81d20de27b9
naomu <muramatsu2023@gmail.com>
2024-02-19 17:51:17 +0900

    4th commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,13 +11,10 @@ class OrdersController < ApplicationController
 
   def create
     #１つの商品を２人のユーザーが同時に5個ずつ注文した際に、その商品の注文数が合計10個になるように悲観的ロックを実装する
-    ActiveRecord::Base.transaction do
-      @order = Order.lock.find_by(user_id: current_user.id)
       @order = current_user.orders.build(order_params)
       @order.save!
       @order.update_total_quantity
       # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
-    end
     redirect_to orders_path
   end
 
@@ -27,3 +24,5 @@ class OrdersController < ApplicationController
     params.require(:order).permit(ordered_lists_attributes: [:item_id, :quantity])
   end
 end
+
+#"order"=>{"ordered_lists_attributes"=>{"0"=>{"quantity"=>"3", "item_id"=>"1"}, "1"=>{"quantity"=>"0", "item_id"=>"2"}, "2"=>{"quantity"=>"0", "item_id"=>"3"}, "3"=>{"quantity"=>"0", "item_id"=>"4"}, "4"=>{"quantity"=>"0", "item_id"=>"5"}, "5"=>{"quantity"=>"0", "item_id"=>"6"}, "6"=>{"quantity"=>"0", "item_id"=>"7"}, "7"=>{"quantity"=>"0", "item_id"=>"8"}, "8"=>{"quantity"=>"0", "item_id"=>"9"}, "9"=>{"quantity"=>"0", "item_id"=>"10"}, "10"=>{"quantity"=>"0", "item_id"=>"11"}}}
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,11 +4,24 @@ class Order < ApplicationRecord
   has_many :items, through: :ordered_lists
   accepts_nested_attributes_for :ordered_lists
 
+  # def create_order_transaction(user, item)
+  #   ActiveRecord::Base.transaction do
+  #     @order = user.orders.build
+  #     @order.save!
+  #     @order.ordered_lists.build(item: item, quantity: 5)
+  #     @order.save!
+  #     @order.update_total_quantity
+  #     # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
+  #   end
+  # end
+
   def update_total_quantity
-    self.ordered_lists.each do |line_item|
-      item = Item.find_by(id: line_item.item_id)
-      item.total_quantity += line_item.quantity
-      item.save!
+    ActiveRecord::Base.transaction do
+      self.ordered_lists.each do |line_item|
+        item = Item.lock.find_by(id: line_item.item_id)
+        item.total_quantity += line_item.quantity
+        item.save!
+      end
     end
   end
 end
